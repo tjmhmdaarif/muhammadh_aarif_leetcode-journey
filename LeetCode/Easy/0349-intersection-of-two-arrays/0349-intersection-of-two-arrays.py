275c6191537efab15cf1dfa61bad2a79505006c3
MUHAMMADH AARIF T J <muhammadhaarif2000@gmail.com>
2026-09-15 14:09:43 +0530

    09-15-2026 0349-intersection-of-two-arrays Easy .py Array

---
+++ LeetCode/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        count = 0
+        result = []
+        seen = {}
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        result = set1 & set2
+        return list(result)
