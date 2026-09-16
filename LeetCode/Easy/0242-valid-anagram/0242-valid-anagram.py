e36181d5c0f6bad2fb6a3c122396137ae9171609
MUHAMMADH AARIF T J <muhammadhaarif2000@gmail.com>
2026-09-16 12:41:56 +0530

    09-16-2026 0242-valid-anagram Easy .py Hash Table

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sorted(s)
-        sorted(t)
-        if sorted(s) == sorted(t):
-            return(True)
-        else:
-            return(False)
+        return sorted(s) == sorted(t)
+        
