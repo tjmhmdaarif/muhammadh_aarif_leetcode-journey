de6eceba040ccee6c5a8e8064c349e3c3acbde09
MUHAMMADH AARIF T J <muhammadhaarif2000@gmail.com>
2026-09-16 12:06:06 +0530

    09-16-2026 0169-majority-element Easy .py Array

---
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        seen = {}
+        for num in nums:
+            if num in  seen:
+                seen[num] += 1
+            else:
+                seen [num] = 1
+        for num in seen :
+            if seen[num] > len (nums) //2:
+                return num
