2ff62e2b1152cda614a1423b55f141f00a9a2981
MUHAMMADH AARIF T J <muhammadhaarif2000@gmail.com>
2026-09-19 14:58:57 +0530

    09-19-2026 0009-palindrome-number Easy .py Math

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
@@ -1,3 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[:: -1]
+        if x < 0:
+            return False
+        reversed_num = 0
+        original = x
+
+        while x > 0:
+            last_digit = x % 10
+            reversed_num = reversed_num * 10 + last_digit
+            x = x // 10
+        return original == reversed_num
