6e4a687d0ea550903dc468a38d5b1a595990fbed
MUHAMMADH AARIF T J <muhammadhaarif2000@gmail.com>
2026-09-19 14:10:58 +0530

    09-19-2026 0066-plus-one Easy .py Array

---
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        for i in range(len(digits) -1 , -1, -1):
+            digits[i] += 1
+
+            if digits[i] == 10:
+                digits[i] = 0
+            else:
+                return digits
+        digits.insert(0, 1)
+        return digits
