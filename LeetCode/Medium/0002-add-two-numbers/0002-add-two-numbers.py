0447c12ad16bc06f25e19899ebe01615a8dac9a8
MUHAMMADH AARIF T J <muhammadhaarif2000@gmail.com>
2026-09-23 11:05:05 +0530

    09-23-2026 0002-add-two-numbers Medium .py Linked List

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,27 +1,21 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-
         dummy = ListNode(0)
         current = dummy
         carry = 0
 
         while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
 
-            digit1 = l1.val if l1 else 0
-            digit2 = l2.val if l2 else 0
-
-            total = digit1 + digit2 + carry
-
+            total = val1 + val2 + carry
             digit = total % 10
             carry = total // 10
 
             current.next = ListNode(digit)
             current = current.next
 
-            if l1:
-                l1 = l1.next
-
-            if l2:
-                l2 = l2.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
         return dummy.next
